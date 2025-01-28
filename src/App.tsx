d89ebeea8f4b8efc2d4b0753b6b6ea8e537c6267
Katona Tamás <katonatamas@Katona-MacBook-Air.local>
2025-01-28 14:31:07 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-2xl font-display">Hello World</h1>
+    </>
+  );
+}
+
+export default App;
